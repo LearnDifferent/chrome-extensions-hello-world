0d7d8d08dac94a6f9e5f64111b95b6d0921d4203
zhou <imzhouhepeng@outlook.com>
2022-06-21 23:01:21 +0800

    learn: 移动书签，触发从 background script 发送消息到 content script

--- js/sendAndGetMessage.js
+++ js/sendAndGetMessage.js
@@ -1,8 +1,13 @@
 // 在窗口加载的时候，发送一条消息，并搭载回调函数
 window.onload = testMessage();
-
 function testMessage() {
     chrome.runtime.sendMessage(
         {msg: "Hello from a content"},
         () => console.log(2 + 2))
 }
+
+// 监听从 background 发送的消息
+chrome.runtime.onMessage.addListener((message, sender, resp) => {
+    console.log("消息：", message);
+    console.log("发送者：", sender);
+})
--- listen.js
+++ listen.js
@@ -4,3 +4,22 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     console.log("sender 表示发送者：" + sender);
     console.log("response 表示会执行回调函数，并将结果返回到 content script 中" + sendResponse());
 })
+
+// 在移动书签的时候，从 background 发送消息到 content script 中
+chrome.bookmarks.onMoved.addListener(() => {
+    // 参数1：搜索活跃的 tabs 和当前的 tab
+    // 参数2：一个 callback，发送消息到某个 tab 中（这里的 tabs 表示所有符合条件的标签页）
+    chrome.tabs.query(
+        {active: true, currentWindow: true},
+        tabs => {
+            // 在该 callback 中，
+            // 参数1 表示表示发送到哪个 tab 中（根据 ID 判断），
+            // 参数2表示发送的消息，
+            // 还可以有参数3 的 callback
+            chrome.tabs.sendMessage(
+                tabs[0].id,
+                {msg: "这是一个消息"}
+            )
+        }
+    )
+})
