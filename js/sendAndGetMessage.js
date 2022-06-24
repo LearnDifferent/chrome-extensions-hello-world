e4d3ec5439648b2d9fd4e14c6415568bad2963e1
zhou <zhou.h.p@outlook.com>
2022-06-24 10:15:13 +0800

    learn: 加深理单向的消息传递

--- js/sendAndGetMessage.js
+++ js/sendAndGetMessage.js
@@ -3,11 +3,24 @@ window.onload = testMessage();
 function testMessage() {
     chrome.runtime.sendMessage(
         {msg: "Hello from a content"},
-        () => console.log(2 + 2))
+        () => console.log(2 + 2)
+    )
 }
 
-// 监听从 background 发送的消息
-chrome.runtime.onMessage.addListener((message, sender, resp) => {
-    console.log("消息：", message);
-    console.log("发送者：", sender);
+// 监听发送到当前页面的消息
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    console.log(sender.tab ?
+        "如果 sender 参数中有 tab 的属性，表示 from a content script:" + sender.tab.url :
+        "如果 sender 参数中没有 tab 的属性，则表示 from the extension");
+
+    // message 参数表示传递过来的 Json 消息
+    if (message.someMessages === "Here is some messages") {
+        // sendResponse 是 callback，可以直接返回 Json 作为回复
+        sendResponse({theResponse: "Here is the response"})
+    }
+    /*
+    In the above example, `sendResponse` was called synchronously.
+    If you want to asynchronously use sendResponse,
+    add `return true;` to the onMessage event handler.
+     */
 })
--- listen.js
+++ listen.js
@@ -5,20 +5,25 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     console.log("response 表示会执行回调函数，并将结果返回到 content script 中" + sendResponse());
 })
 
+// Query tab
+let queryInfo = { active: true, currentWindow: true };
 // 在移动书签的时候，从 background 发送消息到 content script 中
 chrome.bookmarks.onMoved.addListener(() => {
-    // 参数1：搜索活跃的 tabs 和当前的 tab
+    // 参数1：当前的 queryInfo 表示搜索活跃且被当前选中 tabs（selected tab）
     // 参数2：一个 callback，发送消息到某个 tab 中（这里的 tabs 表示所有符合条件的标签页）
     chrome.tabs.query(
-        {active: true, currentWindow: true},
+        queryInfo,
         tabs => {
             // 在该 callback 中，
             // 参数1 表示表示发送到哪个 tab 中（根据 ID 判断），
             // 参数2表示发送的消息，
-            // 还可以有参数3 的 callback
+            // 参数3 的 callback，获取 response
             chrome.tabs.sendMessage(
                 tabs[0].id,
-                {msg: "这是一个消息"}
+                {someMessages: "Here is some messages"},
+                (response) => {
+                    console.log(response);
+                }
             )
         }
     )
