180a01a54fad77c746ce0178a1b46d8c797cc59f
zhou <zhou.h.p@outlook.com>
2022-07-28 11:19:37 +0800

    learn: 只在某些页面才显示右键菜单按钮

--- background.js
+++ background.js
@@ -7,16 +7,16 @@ chrome.contextMenus.create({
     title: "This is a title",
     // ID
     id: "id1",
-    // 作用范围
+    // 右键菜单的作用范围
     contexts: ["all", "selection", "link"],
     // 单击事件
-    onclick: function (t) {
-        // 这里的参数 t 表示被单击的对象的数据
+    onclick: function (onClickData) {
+        // 这里的参数 onClickData 表示被单击的“右键菜单”对象的数据
         // 注意，这个是在「背景页」里面才看得到输出
-        console.log(t);
+        console.log(onClickData);
     }
 }, function () {
-    // 暂时不写 callback
+    console.log("创建右键菜单");
 });
 
 // 这里创建一个 checkbox，作为 id1 菜单的 child
@@ -28,13 +28,14 @@ var checkBoxValues = {
     parentId: "id1",
     contexts: ["selection"],
     // 复制选中文本，并打开百度翻译
-    onclick: function (text){
+    onclick: function (text) {
         // selectionText 是数据中，被选中的文本属性
         window.open("https://fanyi.baidu.com/?aldtype=16047#en/zh/" +
             text.selectionText);
     }
 };
-chrome.contextMenus.create(checkBoxValues, function () {});
+chrome.contextMenus.create(checkBoxValues, function () {
+});
 
 // 这里创建一个 radio，作为 id1 菜单的 child，并且添加改变 id1 的功能
 chrome.contextMenus.create({
@@ -47,5 +48,16 @@ chrome.contextMenus.create({
             title: "id1 has been changed"
         })
     }
+}, function () {
+});
+
+// 只在某些页面才会显示的右键菜单按钮
+chrome.contextMenus.create({
+    type: "normal",
+    title: "Only be shown in some websites",
+    id: "id2",
+    contexts: ["all"],
+    // 能够显示此右键菜单按钮的网站
+    documentUrlPatterns: ["https://www.baidu.com/*", "https://www.apple.com.cn/*"],
 }, function () {
 });
